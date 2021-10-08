75999d2006dc1869cc0f72321b88c8b07c42edd0
marvahm12 <89638711+marvahm12@users.noreply.github.com>
2021-10-08 11:12:47 +0500

    Update print_data_gender.py

--- Pandas/print_data_gender.py
+++ Pandas/print_data_gender.py
@@ -9,11 +9,12 @@ def obesity(excelfile="Obes-phys-acti-diet-eng-2014-tab.xls", nogui = False):
 
     # Define the columns to be read
     columns1 = ['year', 'total', 'males', 'females']
-    print(columns1)
+    #print(columns1)
 
     data_gender = data.parse(u'7.1', skiprows=4, skipfooter=14, names=columns1) 
     print (data_gender)
     print('hello')
-
-    if __name__ == "__main__":
+    
+    
+if __name__ == "__main__":
         obesity()

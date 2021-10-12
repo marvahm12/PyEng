35069f5272c6160f4f9b14131950cb60a5928b0f
marvahm12 <89638711+marvahm12@users.noreply.github.com>
2021-10-12 11:36:33 +0500

    Add files via upload

--- Image_Video/image_video_newcode/display_image2.py
+++ Image_Video/image_video_newcode/display_image2.py
@@ -3,16 +3,16 @@ import sys
 import matplotlib.pyplot as plt
 
 
-def display(infile, nogui=True):
+def display(infile, nogui=False):
     image = cv2.imread(infile)
     if nogui:
         cv2.imwrite('test_display2.png', image)
     else:
-        cv2.imshow("Image", image)
-        cv2.waitKey(0)
+        cv2.imwrite("Image.png", image)
+        #cv2.waitKey(0)
 
 if __name__ == "__main__":
     # Read the image. The first command line argument is the image
 
     display(sys.argv[1])
-    plt.savefig('output/test_display2.png')
+    plt.savefig('output/Image.png')

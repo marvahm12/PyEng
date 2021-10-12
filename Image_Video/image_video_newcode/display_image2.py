0e766b050a56f5fc0037d981d1ec6d9d7bb978a4
marvahm12 <89638711+marvahm12@users.noreply.github.com>
2021-10-12 12:06:51 +0500

    Update display_image2.py

--- Image_Video/image_video_newcode/display_image2.py
+++ Image_Video/image_video_newcode/display_image2.py
@@ -3,13 +3,9 @@ import sys
 import matplotlib.pyplot as plt
 
 
-def display(infile, nogui=False):
+def display(infile):
     image = cv2.imread(infile)
-    if nogui:
-        cv2.imwrite('test_display2.png', image)
-    else:
-        cv2.imwrite("Image.png", image)
-        #cv2.waitKey(0)
+    cv2.imwrite("Image.png", image)
 
 if __name__ == "__main__":
     # Read the image. The first command line argument is the image
